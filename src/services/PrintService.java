519570eefafe8e651d7a2f56e15c7cb25b469482
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-25 21:57:47 -0300

    generics pegando o primeiro elemento digitado

--- src/services/PrintService.java
+++ src/services/PrintService.java
@@ -24,4 +24,15 @@ public class PrintService {
 		return list.get(0);
 	}
 	
+	public void print() {
+		System.out.print("[");
+		if(!list.isEmpty()) {
+			System.out.print(list.get(0));
+		}
+		for (int i = 1; i < list.size(); i++) {
+			System.out.print(", " + list.get(i));
+		}
+		System.out.println("]");
+	}
+	
 }
